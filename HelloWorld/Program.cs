0369b62145bad88d18a4503be613ea4a9afa9a49
X0lotl <danil200319771@gmail.com>
2023-02-19 10:11:12 +0200

    shit commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class HelloWorld
+{
+  static void Main(string[] args)
+  {
+    int[] array = new int[10];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+      array[i] = random.Next(1, 100);
+    }
+    
+    foreach (int number in array)
+    {
+      Console.WriteLine(number);
+    }
+  }
+}
